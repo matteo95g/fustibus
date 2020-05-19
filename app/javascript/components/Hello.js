d9aa5938c7cc9ab6affaba1d18ca89470b194d2c
Felipe Fava <felipe.fava@moove-it.com>
2020-05-19 20:20:00 -0300

    Add Chakra UI

--- app/javascript/components/Hello.js
+++ app/javascript/components/Hello.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { Box, Flex, Input } from "@chakra-ui/core";
 
-const Hello = () => {
-  return <div>HOLA</div>;
-};
+const Hello = ({ title }) => (
+  <Box>
+    <Box p={10} mb={4} bg="green.500" fontSize="2xl" textAlign="center">{title}</Box>
+    <Flex justifyContent="center">
+      <Input
+        w="50%"
+        name="name"
+        placeholder="name"
+      />
+    </Flex>
+  </Box>
+);
 
 export default Hello;
