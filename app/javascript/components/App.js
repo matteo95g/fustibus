fd9667a35b3018871aafb85a5be361af71ef23d1
Felipe Fava <felipe.fava@moove-it.com>
2020-05-19 20:20:00 -0300

    Add Chakra UI

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,14 +1,20 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { ThemeProvider, CSSReset } from "@chakra-ui/core";
+
 import Hello from "@components/Hello";
+import customTheme from '../chakra/theme';
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" render={() => <Hello />} />
-      </Switch>
-    </BrowserRouter>
+    <ThemeProvider theme={customTheme}>
+      <CSSReset />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" render={() => <Hello title="Hello!" />} />
+        </Switch>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
 
