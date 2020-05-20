ec5dc312cbdf2316fa442def210361faaa577bc4
Matteo Guerrieri <matteo.guerrieri@moove-it.com>
2020-05-20 19:40:15 -0300

    Add redux and create store with combineReducers

--- app/javascript/components/Hello/Hello.jsx
+++ app/javascript/components/Hello/Hello.jsx
@@ -3,13 +3,11 @@ import { Box, Flex, Input } from "@chakra-ui/core";
 
 const Hello = ({ title }) => (
   <Box>
-    <Box p={10} mb={4} bg="green.500" fontSize="2xl" textAlign="center">{title}</Box>
+    <Box p={10} mb={4} bg="green.500" fontSize="2xl" textAlign="center">
+      {title}
+    </Box>
     <Flex justifyContent="center">
-      <Input
-        w="50%"
-        name="name"
-        placeholder="name"
-      />
+      <Input w="50%" name="name" placeholder="name" />
     </Flex>
   </Box>
 );
