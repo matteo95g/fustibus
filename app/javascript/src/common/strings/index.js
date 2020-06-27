a36ef89883236deca62f2490ad9368e998ab993f
Jose Aguerre <jose.aguerre@moove-it.com>
2020-06-27 18:11:35 -0300

    Home improvements
     - TODO check user for current club

---
+++ app/javascript/src/common/strings/index.js
@@ -0,0 +1,13 @@
+const strings = {
+  // Home
+  homeTitle: '¡Bienvenido a Club de Ciencia!',
+  homeSubtitle: 'Aún no eres participante de ningún club.',
+  homeShuffle: 'Busca un club que te interese y envía una solicitud de participación utilizando',
+  homeCreate: 'También puedes crear un nuevo club presionando',
+
+  // Actions
+  create: 'Crear',
+  shuffle: 'Shuffle',
+};
+
+export default strings;
