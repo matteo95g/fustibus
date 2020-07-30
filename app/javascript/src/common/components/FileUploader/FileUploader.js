480b596d96da2b097400a64942a83b9a5a0e67e0
Jose Aguerre <jose.aguerre@moove-it.com>
2020-07-30 19:38:50 -0300

    - Add file image preview in club creation

--- app/javascript/src/common/components/FileUploader/FileUploader.js
+++ app/javascript/src/common/components/FileUploader/FileUploader.js
@@ -1,15 +1,18 @@
-import React, { useCallback, useState } from "react";
+import React, { useCallback, Fragment, useState, useEffect } from "react";
 import { useDropzone } from "react-dropzone";
-import { Box } from "@common/ui";
+import { Box, Image } from "@common/ui";
 
 const ACCEPTED_FILES = ["image/*"];
 
 const FileUploader = ({ handleUpload, multiple, uploading = false }) => {
+  const [preview, setPreview] = useState(null);
+
   const onDropAccepted = useCallback((acceptedFiles) => {
     let files = new Array();
 
     acceptedFiles.forEach((file, index) => {
       file.preview = URL.createObjectURL(file);
+      setPreview(file.preview);
       const reader = new FileReader();
       reader.readAsDataURL(file);
       reader.onload = () => {
@@ -36,15 +39,25 @@ const FileUploader = ({ handleUpload, multiple, uploading = false }) => {
     accept: ACCEPTED_FILES,
   });
 
+  useEffect(
+    () => () => {
+      URL.revokeObjectURL(preview);
+    },
+    [preview]
+  );
+
   return (
-    <Box pointerEvents={uploading ? "none" : ""} my="4" {...getRootProps({ className: "dropzone" })}>
-      <input {...getInputProps()} />
-      {isDragActive ? (
-        <p>Suelta los archivos aqui...</p>
-      ) : (
-        <p>Arrastra los archivos aqui, o clickea para seleccionar</p>
+    <Fragment>
+      <Box pointerEvents={uploading ? "none" : ""} my="4" {...getRootProps({ className: "dropzone" })}>
+        <input {...getInputProps()} />
+        {isDragActive ? <p>Suelta el archivo aqui...</p> : <p>Arrastra el archivo aqui, o clickea para seleccionar</p>}
+      </Box>
+      {preview && (
+        <Box my="3">
+          <Image size="100px" objectFit="contain" src={preview} />
+        </Box>
       )}
-    </Box>
+    </Fragment>
   );
 };
 
