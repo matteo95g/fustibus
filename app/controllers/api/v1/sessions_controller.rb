7b90904d2b30152b8e48a190d8aa2a5e443cec2e
Felipe Fava <felipe.fava@moove-it.com>
2020-08-06 19:34:41 -0300

    Fix devise sessions controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,6 @@
 module Api
   module V1
-    class Api::V1::SessionsController < Devise::SessionsController
+    class SessionsController < ::Devise::SessionsController
       def create
         self.resource = warden.authenticate!(auth_options)
         set_flash_message(:notice, :signed_in) if is_navigational_format?
