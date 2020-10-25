effd57775295a2945e726eef6888c465649edb13
Matteo Guerrieri <matteo.guerrieri@moove-it.com>
2020-10-24 21:58:16 -0300

    Send invite email

--- app/javascript/src/app/middlewares.js
+++ app/javascript/src/app/middlewares.js
@@ -1,5 +1,5 @@
 import history from "./history";
-import { loginUrl } from "@utils/app/urlHelpers";
+import { loginUrl, signupUrl } from "@utils/app/urlHelpers";
 import { LOGIN, logout } from "@features/users/usersSlice";
 
 const UNAUTHORIZED_STATUS = 401;
@@ -8,6 +8,9 @@ export const unauthorizeHandler = (store) => (next) => (action) => {
   const httpStatus = action.payload?.status;
   const isLogin = action.type === LOGIN;
   const errorKey = action.payload?.data?.errorKey;
+
+  const urlParams = new URLSearchParams(window.location.search);
+  if (urlParams.has("invited_email")) history.push(`${signupUrl()}?invited_email=${urlParams.get("invited_email")}`);
 
   if (httpStatus === UNAUTHORIZED_STATUS) {
     if (errorKey === "expired_token") {
--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,17 @@ module Fustibus
 
     config.autoload_paths.push Rails.root.join("app", "models", "images")
 
+    config.action_mailer.delivery_method = :smtp
+    config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                 587,
+      domain:               'gmail.com',
+      user_name:            ENV['SMTP_USERNAME'],
+      password:             ENV['SMTP_PASSWORD'],
+      authentication:       :login,
+      enable_starttls_auto: true
+    }
+
     config.generators do |g|
       g.system_tests = nil
       g.test_framework :rspec
@@ -31,4 +42,7 @@ module Fustibus
 
     config.i18n.default_locale = :es
   end
+
+  Rails.application.routes.default_url_options[:host] = ENV['DEFAULT_URL_HOST'] || 'localhost:3000'
+  Rails.application.routes.default_url_options[:protocol] = ENV['DEFAULT_URL_PROTOCOL'] || 'http'
 end
