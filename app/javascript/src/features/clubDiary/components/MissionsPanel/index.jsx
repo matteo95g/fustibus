fba1328f1e093d76a0fcc7b90d64d4011d863475
Matteo Guerrieri <matteo.guerrieri@moove-it.com>
2020-11-07 19:58:52 -0300

    Styling

--- app/javascript/src/features/clubDiary/components/MissionsPanel/index.jsx
+++ app/javascript/src/features/clubDiary/components/MissionsPanel/index.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-
 import * as Yup from "yup";
 import { REQUIRED, COMPLETE } from "@app/constants";
 import { listMissions, createMission, updateMission, deleteMission } from "@features/clubDiary/clubDiarySlice";
@@ -11,10 +10,12 @@ import DeleteButton from "@common/components/DeleteButton";
 import EditButton from "@common/components/EditButton";
 import MissionsList from "@features/clubDiary/components/MissionsList";
 import MissionDetail from "@features/clubDiary/components/MissionDetail";
-import { Skeleton, Flex, Stack, Button } from "@common/ui";
+import { Skeleton, Flex, Stack, Button, Text } from "@common/ui";
 import { LOADING } from "@app/constants";
 import { currentUser } from "@features/users/selectors";
 import { useDispatch, useSelector } from "react-redux";
+import SaveButton from "@common/components/SaveButton";
+import CancelButton from "@common/components/CancelButton";
 
 const MissionsPanel = ({}) => {
   const dispatch = useDispatch();
@@ -85,6 +86,12 @@ const MissionsPanel = ({}) => {
     attributes: { isCounselor },
   } = useSelector(currentUser);
 
+  const handleCancel = () => {
+    setEditingMission(false);
+    setMission(null);
+    setNewMission(false);
+  };
+
   return (
     <>
       <Flex pt="4" justifyContent="space-between" alignItems="center">
@@ -104,7 +111,7 @@ const MissionsPanel = ({}) => {
       </Flex>
 
       <Skeleton isLoaded={isLoaded}>
-        <Flex mt="4">
+        <Flex mt="4" flexDirection={{ base: "column", md: "row" }}>
           <MissionsList
             missions={missions}
             onSelect={onMissionSelected}
@@ -112,12 +119,12 @@ const MissionsPanel = ({}) => {
           />
           {!newMission && !editingMission && selectedMission && <MissionDetail mission={selectedMission} />}
           {!newMission && !selectedMission && (
-            <Flex flex="1" ml="4">
-              Selecciona una misión para ver en detalle.
+            <Flex flex="1" ml="4" alignItems="center" justifyContent="center">
+              <Text fontSize="xl">Selecciona una misión para ver en detalle.</Text>
             </Flex>
           )}
           {(newMission || editingMission) && (
-            <Flex flex="1" alignSelf="stretch" ml="4" flexDirection="column" borderColor="black" borderWidth="0.5px">
+            <Flex flex="1" alignSelf="stretch" ml="4" flexDirection="column">
               <Formik
                 initialValues={
                   editingMission
@@ -133,9 +140,8 @@ const MissionsPanel = ({}) => {
                   <FormikTextInput my="4" name="name" type="text" placeholder="Nombre" />
                   <FormikTextArea my="4" name="description" type="text" placeholder="Descripción" />
 
-                  <Button type="submit" variantColor="green" isLoading={createMissionStatus == LOADING}>
-                    Guardar
-                  </Button>
+                  <SaveButton mr="2" type="submit" isLoading={createMissionStatus == LOADING} />
+                  <CancelButton onClick={handleCancel} />
                 </Form>
               </Formik>
             </Flex>
