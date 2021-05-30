c50fc91cd88afc591f49f7849ed3bc876cc9baab
Felipe Fava <felipe.fava@moove-it.com>
2021-05-30 17:24:57 -0300

    Use sendgrid as SMTP provider

--- config/application.rb
+++ config/application.rb
@@ -23,12 +23,13 @@ module Fustibus
 
     config.action_mailer.delivery_method = :smtp
     config.action_mailer.smtp_settings = {
-      address:              'smtp.gmail.com',
+      user_name:            'apikey',
+      password:             ENV['SENDGRID_PASSWORD'],
+      domain:               'sendgrid.com',
+      address:              'smtp.sendgrid.net',
       port:                 587,
-      domain:               'gmail.com',
-      user_name:            ENV['SMTP_USERNAME'],
-      password:             ENV['SMTP_PASSWORD'],
-      authentication:       :login
+      authentication:       :plain,
+      enable_starttls_auto: true,
     }
 
     config.generators do |g|
